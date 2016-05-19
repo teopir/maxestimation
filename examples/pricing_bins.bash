c4b51ec240ee7fa2282b1038ce3e698d3647d0ea
Matteo Pirotta <matteo.pirotta@polimi.it>
2016-05-19 18:08:03 +0200

    Updated

--- examples/pricing_bins.bash
+++ examples/pricing_bins.bash
@@ -1,12 +1,14 @@
 #!/bin/bash -x
 
-for i in {900..999}
+folder='pricing_results/noweighted_gamma'
+
+for i in {900..920}
 do
-	for b in {5..50..5}
+	for b in {2..10..2}
 	do
 		for d in 100 200 300 400 500
 		do
-			time python3.5 pricing_est.py --nbins=${b} --nsamples=${d} --suffix=${i} --exclude_weighted=True --folder='pricing_results/noweighted'
+			time python3.5 pricing_est.py --nbins=${b} --nsamples=${d} --suffix=${i} --exclude_weighted=True --folder=${folder}
 		done
 	done
 done
